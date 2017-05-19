0b85124a408ca119680d64e6e641e89d65755d5d
Patonix <patricio.montecinos@gmail.com>
2017-05-19 10:58:47 -0400

    ejemplo pull request

--- EjemploGit.java
+++ EjemploGit.java
@@ -7,12 +7,6 @@ public class EjemploGit {
 		System.out.println("Hola Mundo");
 		System.out.println("Prueba 1");
 		System.out.println("Prueba 2");
-		System.out.println("Prueba en eclipse BCI");
-		System.out.println("Prueba en eclipse BCI --2");
-		System.out.println("Ejemplo para probar merge");
-		System.out.println("Modificaciones desde Github");
-		System.out.println("Se hace un cambio desde el depto");
-		System.out.println("Se hace un cambio desde el Github3");
 
 	}
 	
