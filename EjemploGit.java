6bbf98d9afb6d1c563980624aa60e84832389815
Patonix <patricio.montecinos@gmail.com>
2017-05-22 20:28:32 -0400

    Update EjemploGit.java
    Prueba de fetch

--- EjemploGit.java
+++ EjemploGit.java
@@ -4,15 +4,7 @@ public class EjemploGit {
 	
 	public void Hola()
 	{
-		System.out.println("Hola Mundo");
-		System.out.println("Prueba 1");
-		System.out.println("Prueba 2");
-		System.out.println("Prueba en eclipse BCI");
-		System.out.println("Prueba en eclipse BCI --2");
-		System.out.println("Ejemplo para probar merge");
-		System.out.println("Modificaciones desde Github");
-		System.out.println("Se hace un cambio desde el depto");
-		System.out.println("Se hace un cambio desde el Github3");
+	
 
 	}
 	
